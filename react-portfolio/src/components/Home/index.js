41b7f36327b88c531c87d98f4c493504e926574e
FundaGungorAlkan <fundaalkan112@gmail.com>
2024-05-22 14:18:46 +0300

    -

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,4 +1,34 @@
-const Home =() =>('Hello')
+import LogoTitle from '../../assets/images/main-logo.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+
+const Home =() =>{
+
+
+return(
+<div className="container home-page">
+<div className="text-zone">
+<h1>Hi, <br/> I'm
+<img src={LogoTitle} alt="developer"/>
+lobodan
+<br/>
+web developer
+</h1>
+<h2>Frontend Developer</h2>
+<Link to="/contact" className='flat-button'>CONTACT ME</Link>
+
+</div>
+
+</div>
+
+
+
+
+
+)
+
+}
 
 
 
